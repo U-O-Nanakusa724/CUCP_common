87a6136fe149e1f6449b955ffc8c4c9fa2b0a3e8
“U-O-Nanakusa724“ <u.o.nanakusa724.engineer@gmail.com>
2021-02-03 15:50:16 +0900

    [add]commonにいくつかのクラスを引っ越し
    該当のクラスを中身ほぼそのままにして移動

---
+++ cucp_common/src/main/java/biz/uoray/common/repository/PriceRepository.java
@@ -0,0 +1,9 @@
+package biz.uoray.common.repository;
+
+import biz.uoray.common.entity.Price;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PriceRepository extends JpaRepository<Price, Integer> {
+}
