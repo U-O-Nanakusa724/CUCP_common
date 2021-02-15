43bf3e811a85c531844d15399b0dfc3074aaa211
“U-O-Nanakusa724“ <u.o.nanakusa724.engineer@gmail.com>
2021-02-15 17:41:12 +0900

    [fix]common-6 論理削除の絞り込み
    carのみ対応

--- cucp_common/src/main/java/biz/uoray/cucp/repository/CarRepository.java
+++ cucp_common/src/main/java/biz/uoray/cucp/repository/CarRepository.java
@@ -1,9 +1,15 @@
 package biz.uoray.cucp.repository;
 
 import biz.uoray.cucp.entity.Car;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CarRepository extends JpaRepository<Car, Integer> {
+public interface  CarRepository extends JpaRepository<Car, Integer> {
+
+    @Query(value = "SELECT c FROM Car c WHERE c.deletedAt IS NULL")
+    Page<Car> findActive(Pageable pageable);
 }
